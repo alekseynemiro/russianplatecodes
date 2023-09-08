49e480728954c0a3166d7e19c47e19f00ebc1a4d
Aleksey Nemiro <aleksey@kbyte.ru>
2023-09-08 22:51:42 +0300

    Create ApplicationDrawer

---
+++ lib/ui/views/application_drawer.dart
@@ -0,0 +1,65 @@
+
+import 'package:flutter/material.dart';
+
+import '../pages/dictionary_page.dart';
+import '../pages/home_page.dart';
+
+class ApplicationDrawer extends StatelessWidget {
+
+  const ApplicationDrawer({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              image: const DecorationImage(
+                image: AssetImage('assets/bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Text(
+                  'Автомобильные коды России',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text('Справочник кодов'),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Справочник регионов'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const DictionaryPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+}
